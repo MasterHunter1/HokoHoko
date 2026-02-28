ebd26afddbbf43be022bc6cb25df9654b5566a63
MasterHunter1 <85642882+MasterHunter1@users.noreply.github.com>
2026-02-28 20:41:55 +1300

    Add stub functions for goBack(), sendVerificationCode(), verifyCode(), and startChat() in app.js

---
+++ app.js
@@ -0,0 +1,7 @@
+function goBack() { alert('goBack() called'); }
+
+function sendVerificationCode() { alert('sendVerificationCode() called'); }
+
+function verifyCode() { alert('verifyCode() called'); }
+
+function startChat() { alert('startChat() called'); }
